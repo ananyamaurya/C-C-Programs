ef17354a0c2268a46f97f0ea0888e8f1da1c3cfe
ananyamaurya <36106977+ananyamaurya@users.noreply.github.com>
2021-02-13 12:39:15 +0530

    Round Robin

---
+++ Operating Systems/RoundRobin/cmake-build-debug/CMakeFiles/RoundRobin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RoundRobin.dir/main.cpp.obj"
+  "RoundRobin.exe"
+  "RoundRobin.exe.manifest"
+  "RoundRobin.pdb"
+  "libRoundRobin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RoundRobin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
