9c106f5bd61448f3a4f6216eac8c6ceebb3c545c
ananyamaurya <36106977+ananyamaurya@users.noreply.github.com>
2021-03-07 12:50:20 +0530

    RSA

---
+++ Network Security and Cryptography/RSA/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+bool checkPrime(int n){
+    for(int i=2;i<n/2;i++){
+        return  (n % i != 0);
+    }
+}
+int findE(int n){
+    for(int i=2;i<n/2;i++){
+        if(n%i!=0){
+            return i;
+        }
+    }
+}
+int findD(int e,int pn){
+    for(int i=2; i<pn;i++){
+        if((i*e)%pn ==1){
+            return i;
+        }
+    }
+}
+int main() {
+    long long int p,q,d,e,m,c,fn,pn;
+    L1: cout<<"Enter two Prime No.";
+        cin>>p;
+        cin>>q;
+        if(checkPrime(p) && checkPrime(q)){
+            fn = p*q;
+            pn = (p-1) * (q-1);
+            e = findE(pn);
+            d = findD(e,pn);
+            cout<<"Keys are :: "<<e<<" "<<d;
+            m=88;
+            c=pow(m,e);
+            c=c%fn;
+            cout<<"\n"<<c;
+        }
+        else{
+            goto L1;
+        }
+}
